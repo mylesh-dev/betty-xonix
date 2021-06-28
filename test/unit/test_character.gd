76d8478a6f6b355fef9180122c6aadb798c29281
mylesh <mylesh.dev@gmail.com>
2021-06-28 20:16:45 +0300

    #0. Simplify code

--- test/unit/test_character.gd
+++ test/unit/test_character.gd
@@ -1,16 +1,5 @@
 extends 'res://addons/gut/test.gd'
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func test_ready():
-	pass; # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass;
